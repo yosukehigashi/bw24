98b2bb643fab6f67aec6c1d25401c014442f97b8
Tomas Uhrik <tomasuhrik@ondaytrip.com>
2024-03-30 14:40:39 +0900

    sharing context between pages

---
+++ frontend/src/app/GetVenueForm.tsx
@@ -0,0 +1,63 @@
+"use client"
+
+import React from 'react';
+
+import { Button } from "@fluentui/react-button";
+import { Input } from "@fluentui/react-input";
+import { Label } from "@fluentui/react-label";
+import { Spinner } from "@fluentui/react-spinner";
+import { Display } from "@fluentui/react-text";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useId } from "@fluentui/react-components";
+import styles from "./page.module.css";
+import { useVenueContext } from './venueProvider';
+
+export const GetVenueForm = () => {
+    const router = useRouter()
+    const [url, setUrl] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const largeId = useId("input-large");
+    const { value, setValue } = useVenueContext();
+  
+    const handleFetchVenue = async () => {
+      const venueId = url.replace("https://www.instabase.jp/space/", "");
+      fetchVenue(venueId?.toString() || "");
+    }
+  
+    const fetchVenue = async (venueId: string) => {
+      setIsLoading(true);
+      const response = await fetch(`http://127.0.0.1:8080/venue/${venueId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        },
+      });
+  
+      const data = await response.json();
+      console.log('venueId:', venueId);
+      setIsLoading(false);
+      console.log('response:', data);
+      setValue({ id: venueId, initialImageUrls: data?.urls });
+      router.push(`/venue`);
+    }
+    
+    return (
+        <div className={styles.wrapper}>
+            <Display>Instabase business suite</Display>
+
+            <div className={styles.form}>
+              <div className={styles.wrapperInput}>
+                <Label size="large" htmlFor={largeId}>
+                  Venue url
+                </Label>
+                <Input size="large" id={largeId} onChange={(event) => {
+                  setUrl(event.target.value);
+                }}/>
+              </div>
+
+              <Button appearance="primary" onClick={handleFetchVenue} icon={isLoading ? <Spinner size="tiny" /> : undefined}>Find my venue</Button>
+            </div>
+          </div>
+    );
+}
---
+++ frontend/src/app/venueProvider.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import React, { createContext, useContext, useState } from 'react';
+
+
+type Venue = {
+    id: string;
+    initialImageUrls: string[];
+};
+
+// Step 1: Create the context
+const VenueContext = createContext<{
+    value: Venue | undefined;
+    setValue: (venue: Venue) => void;
+
+}>({
+    value: undefined,
+    setValue: (venue: Venue) => {},
+});
+
+// Step 2: Create a provider component
+const VenueProvider = ({ children }: {children: React.ReactNode}) => {
+    const [value, setValue] = useState<Venue | undefined>();
+    // Your provider logic goes here
+
+    return <VenueContext.Provider value={{
+        value,
+        setValue,
+    }}>{children}</VenueContext.Provider>;
+};
+
+// Step 3: Create a custom hook to access the context
+const useVenueContext = () => {
+    const context = useContext(VenueContext);
+    if (!context) {
+        throw new Error('useMyContext must be used within a MyProvider');
+    }
+    return context;
+};
+
+// Step 4: Export the context and the provider
+export { VenueContext, VenueProvider, useVenueContext };
