d4b75e9e37657ca67028aed78c6948b1335f51e5
Yosuke Higashi <yosuke.higashi1@gmail.com>
2024-03-30 13:46:25 +0900

    flask cors + freeze reqs

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask
 import requests
 from bs4 import BeautifulSoup
+from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 def scrape_listing(room_id):
